68162b4dcf44c1d885a57e5920698a96957a4096
unknown <jugalmehra17@gmail.com>
2023-06-18 23:06:49 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  function handleDelete(event, user){
+    console.log(event);
+    console.log(user);
+
+  }
+
+  function handleEdit(event, user){
+    console.log(event);
+    console.log(user);
+    
+  }
+
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+      setData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>Add contact</button>
+      {data.map((user) => {
+        return (
+          <div key={user.id}>
+            Name: {user.name}<br></br>
+            Username: {user.username}
+            <button onClick={(e)=>{handleEdit(e , user)}}>Edit contact</button>
+            <button onClick={(e)=>{handleDelete(e , user)}}>Delete contact</button>
+
+          </div>
+        );
+      })}
     </div>
   );
 }
